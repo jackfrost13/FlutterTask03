cc0e8acd6665f2dd89cd03593c4ad190c79c47b5
jackfrost13 <rohitagrl1302@gmail.com>
2018-10-03 07:49:05 +0530

    adds json parsing

--- lib/src/appBody.dart
+++ lib/src/appBody.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'flutter_articles.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-List<Data> _data = gendata;
+import 'customList.dart';
+import 'dart:convert';
 
 class AppBody extends StatefulWidget {
   @override
@@ -14,77 +11,27 @@ class AppBody extends StatefulWidget {
 class _AppBodyState extends State<AppBody> {
   @override
   Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: () async {
-        await Future.delayed(Duration(seconds: 1));
-        setState(() {
-          _data.shuffle();
-        });
-        return;
-      },
-      child: ListView.builder(
-        itemCount: _data.length,
-        itemBuilder: (BuildContext context, int index) {
-          return customList(index);
-        },
-      ),
-    );
-  }
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context).loadString('json/Database.json'),
+      builder: (context, snapshot) {
+        var mydata = json.decode(snapshot.data.toString());
+        return RefreshIndicator(
+          onRefresh: () async {
 
-  Widget customList(int index) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(30.0),
-        splashColor: Colors.teal[400],
-        onTap: () async{
-          if(await canLaunch(_data[index].url))
-            launch(_data[index].url);
-        },
-        child: Container(
-          padding: EdgeInsets.all(10.0),
-          child: Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.network(
-                  _data[index].icon,
-                  fit: BoxFit.cover,
-                  height: 50.0,
-                  width: 50.0,
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      _data[index].text,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
+            setState(() {
+              mydata.shuffle();
+            });
+            return await Future.delayed(Duration(seconds: 2));
 
-                        Text(
-                          "By : ${_data[index].by}",
-                        ),
-                        Text(
-                          "Posted : ${_data[index].age}",
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ],
+          },
+          child: ListView.builder(
+            itemCount: mydata == null ? 0 : mydata.length,
+            itemBuilder: (BuildContext context, int index) {
+              return customList(mydata[index]);
+            },
           ),
-        ),
-      ),
+        );
+      },
     );
-//
   }
 }
--- lib/src/flutter_articles.dart
+++ lib/src/flutter_articles.dart
@@ -11,16 +11,17 @@ final gendata = [
       url:
           "https://medium.com/@sidky/using-streambuilder-in-flutter-dcc2d89c2eae",
       by: "Manzurur Rahman Khan",
-      age: "2/1/2018",
+      age: "02/01/2018",
       icon:
           'https://miro.medium.com/fit/c/240/240/1*ar58OUwh0QjXNk776p0USQ.jpeg'),
   Data(
       text: "Simple JSON with Flutter",
       url: "https://flutter.institute/simple-json-with-flutter/",
       by: "Brian Armstrong",
-      age: "3/1/2018",
+      age: "03/01/2018",
       icon:
-          'https://flutter.institute/content/images/2017/11/flutterinstitute.png'),
+          "https://flutter.institute/content/images/2017/11/flutterinstitute.png"
+  ),
   Data(
       text: "Flutter hands on: Building a News App",
       url:
@@ -28,13 +29,14 @@ final gendata = [
       by: "Ankur Kedia",
       age: "26/12/2017",
       icon:
-          'https://cdn-images-1.medium.com/max/140/1*rSGF7OBUjv3Ac2kP_cSjtA@2x.png'),
+          "https://cdn-images-1.medium.com/max/140/1*rSGF7OBUjv3Ac2kP_cSjtA@2x.png"
+  ),
   Data(
       text: 'Using Flutter’s InheritedWidget to hold app state',
       url:
           'https://medium.com/@chemamolins/is-flutters-inheritedwidget-a-good-fit-to-hold-app-state-2ec5b33d023e',
       by: 'Chema Molins',
-      age: '6/1/2018',
+      age: '06/01/2018',
       icon: 'https://cdn-images-1.medium.com/max/800/1*emiGsBgJu2KHWyjluhKXQw.png'
   ),
   Data(
@@ -43,7 +45,7 @@ final gendata = [
     by: "Brian Armstrong",
     icon:
         'https://flutter.institute/content/images/2017/11/flutterinstitute.png',
-    age: '29/1/2018',
+    age: '29/01/2018',
   ),
   Data(
     text: 'Sharing Location using Inherited Widget',
